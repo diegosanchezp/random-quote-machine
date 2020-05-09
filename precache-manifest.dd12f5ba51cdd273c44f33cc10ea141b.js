97156336298454bbf519bd99236035106d3e804c
Diego Sanchez <diegosandmg@gmail.com>
2020-05-08 22:33:22 -0400

    Updates

---
+++ precache-manifest.dd12f5ba51cdd273c44f33cc10ea141b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9f24614f6005af58fc5710a5b9aa94",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "86b4831a468459d34375",
+    "url": "/random-quote-machine/static/css/main.7b4b4c9b.chunk.css"
+  },
+  {
+    "revision": "c5a51983cb18ea277687",
+    "url": "/random-quote-machine/static/js/2.d9a76811.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.d9a76811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b4831a468459d34375",
+    "url": "/random-quote-machine/static/js/main.d18e2dee.chunk.js"
+  },
+  {
+    "revision": "5d10ac826fadbd9fa6e6",
+    "url": "/random-quote-machine/static/js/runtime-main.62554c6d.js"
+  }
+]);
